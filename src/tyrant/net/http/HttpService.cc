2b568c61d3f58d6b56aec27dc3dac980cefb8e1b
tyrantZhao <903014791@qq.com>
2018-11-30 17:09:58 +0800

    1.modify http and websocket module to adjust the net related functions which have been changed in previous commits. 2. add http keep-alive feature

--- src/tyrant/net/http/HttpService.cc
+++ src/tyrant/net/http/HttpService.cc
@@ -1,15 +1,15 @@
-#include <tyrant/net/http/httpservice.h>
+﻿#include <tyrant/net/http/HttpService.h>
 
-#include <string>
 #include <cstring>
 #include <cassert>
+#include <string>
 
 #include <tyrant/common/sha1.h>
 #include <tyrant/common/base64.h>
 #include <tyrant/net/http/http_parser.h>
-#include <tyrant/net/http/websocketformat.h>
+#include <tyrant/net/http/WebSocketFormat.h>
 
-using namespace ::tyrant::net;
+using namespace tyrant::net;
 
 HttpSession::HttpSession(TCPSession::PTR session)
 {
@@ -26,7 +26,7 @@ const TyrantAny& HttpSession::getUD() const
     return mUD;
 }
 
-void HttpSession::setUD(TyrantAny ud)
+void HttpSession::setUD(const TyrantAny& ud)
 {
     mUD = ud;
 }
@@ -71,14 +71,14 @@ const HttpSession::WS_CONNECTED_CALLBACK& HttpSession::getWSConnectedCallback()
     return mWSConnectedCallback;
 }
 
-void HttpSession::send(const DataSocket::PACKET_PTR& packet, 
+void HttpSession::send(const DataSocket::PACKET_PTR& packet,
     const DataSocket::PACKED_SENDED_CALLBACK& callback /* = nullptr */)
 {
     mSession->send(packet, callback);
 }
 
-void HttpSession::send(const char* packet, 
-    size_t len, 
+void HttpSession::send(const char* packet,
+    size_t len,
     const DataSocket::PACKED_SENDED_CALLBACK& callback)
 {
     mSession->send(packet, len, callback);
@@ -106,85 +106,65 @@ HttpSession::PTR HttpSession::Create(TCPSession::PTR session)
     return std::make_shared<make_shared_enabler>(std::move(session));
 }
 
-void HttpService::setup(const TCPSession::PTR& session, 
+void HttpService::setup(const TCPSession::PTR& session,
     const HttpSession::ENTER_CALLBACK& enterCallback)
 {
     auto httpSession = HttpSession::Create(session);
     if (enterCallback != nullptr)
-    {
         enterCallback(httpSession);
-    }
     HttpService::handle(httpSession);
 }
 
-size_t HttpService::ProcessWebSocket(const char* buffer, 
+size_t HttpService::ProcessWebSocket(const char* buffer,
     size_t len,
     const HTTPParser::PTR& httpParser,
     const HttpSession::PTR& httpSession)
 {
-    size_t retlen = 0;
-
     const char* parse_str = buffer;
     size_t leftLen = len;
 
     auto& cacheFrame = httpParser->getWSCacheFrame();
     auto& parseString = httpParser->getWSParseString();
 
-    while (leftLen > 0)
-    {
+    while (leftLen > 0){
         parseString.clear();
 
         auto opcode = WebSocketFormat::WebSocketFrameType::ERROR_FRAME;
         size_t frameSize = 0;
         bool isFin = false;
+
         if (!WebSocketFormat::wsFrameExtractBuffer(parse_str, leftLen, parseString, opcode, frameSize, isFin))
-        {
+            // 如果没有解析出完整的ws frame则退出函数
             break;
-        }
 
-        if ( isFin &&
-            (opcode == WebSocketFormat::WebSocketFrameType::TEXT_FRAME ||
-             opcode == WebSocketFormat::WebSocketFrameType::BINARY_FRAME))
-        {
-            if (!cacheFrame.empty())
-            {
-                cacheFrame += parseString;
-                parseString = std::move(cacheFrame);
-                cacheFrame.clear();
+        // 如果当前frame的fin为false或者opcode为延续包，则将当前frame的payload添加到cache
+        if (!isFin || opcode == WebSocketFormat::WebSocketFrameType::CONTINUATION_FRAME)
+            cacheFrame += std::move(parseString);
+
+        // 如果当前fram的fin为false，并且opcode不为延续包，则表示收到分段payload的第一个段(frame)，需要缓存当前frame的opcode
+        if (!isFin && opcode != WebSocketFormat::WebSocketFrameType::CONTINUATION_FRAME)
+            httpParser->cacheWSFrameType(opcode);
+
+        if (isFin) {
+            // 如果fin为true，并且opcode为延续包，则表示分段payload全部接受完毕，因此需要获取之前第一次收到分段frame的opcode作为整个payload的类型
+            if (opcode == WebSocketFormat::WebSocketFrameType::CONTINUATION_FRAME) {
+                if (!cacheFrame.empty()) {
+                    parseString = std::move(cacheFrame);
+                    cacheFrame.clear();
+                }
+                opcode = httpParser->getWSFrameType();
             }
 
             const auto& wsCallback = httpSession->getWSCallback();
             if (wsCallback != nullptr)
-            {
                 wsCallback(httpSession, opcode, parseString);
-            }
-        }
-        else if (opcode == WebSocketFormat::WebSocketFrameType::CONTINUATION_FRAME)
-        {
-            cacheFrame += parseString;
-            parseString.clear();
-        }
-        else if (opcode == WebSocketFormat::WebSocketFrameType::PING_FRAME ||
-            opcode == WebSocketFormat::WebSocketFrameType::PONG_FRAME ||
-            opcode == WebSocketFormat::WebSocketFrameType::CLOSE_FRAME)
-        {
-            const auto& wsCallback = httpSession->getWSCallback();
-            if (wsCallback != nullptr)
-            {
-                wsCallback(httpSession, opcode, parseString);
-            }
-        }
-        else
-        {
-            assert(false);
         }
 
         leftLen -= frameSize;
-        retlen += frameSize;
         parse_str += frameSize;
     }
 
-    return retlen;
+    return parse_str - buffer;
 }
 
 size_t HttpService::ProcessHttp(const char* buffer,
@@ -192,37 +172,28 @@ size_t HttpService::ProcessHttp(const char* buffer,
     const HTTPParser::PTR& httpParser,
     const HttpSession::PTR& httpSession)
 {
-    const auto retlen = httpParser->tryParse(buffer, len);
-    if (!httpParser->isCompleted())
-    {
-        return retlen;
+    size_t retlen = len;
+    if (!httpParser->isCompleted()) {
+        retlen = httpParser->tryParse(buffer, len);
+        if (!httpParser->isCompleted())
+            return retlen;
     }
 
-    if (httpParser->isWebSocket())
-    {
-        if (httpParser->hasKey("Sec-WebSocket-Key"))
-        {
+    if (httpParser->isWebSocket()) {
+        if (httpParser->hasKey("Sec-WebSocket-Key")){
             auto response = WebSocketFormat::wsHandshake(httpParser->getValue("Sec-WebSocket-Key"));
             httpSession->send(response.c_str(), response.size());
         }
 
         const auto& wsConnectedCallback = httpSession->getWSConnectedCallback();
         if (wsConnectedCallback != nullptr)
-        {
             wsConnectedCallback(httpSession, *httpParser);
-        }
-    }
-    else
-    {
+    } else {
         const auto& httpCallback = httpSession->getHttpCallback();
         if (httpCallback != nullptr)
-        {
             httpCallback(*httpParser, httpSession);
-        }
         if (httpParser->isKeepAlive())
-        {
             httpParser->clearParse();
-        }
     }
 
     return retlen;
@@ -230,31 +201,24 @@ size_t HttpService::ProcessHttp(const char* buffer,
 
 void HttpService::handle(const HttpSession::PTR& httpSession)
 {
-    /*TODO::keep alive and timeout close */
     auto& session = httpSession->getSession();
 
     session->setDisConnectCallback([httpSession](const TCPSession::PTR& session){
         const auto& tmp = httpSession->getCloseCallback();
         if (tmp != nullptr)
-        {
             tmp(httpSession);
-        }
     });
 
     auto httpParser = std::make_shared<HTTPParser>(HTTP_BOTH);
     session->setDataCallback([httpSession, httpParser](
-                                const TCPSession::PTR& session, 
-                                const char* buffer, size_t len){
+                                const TCPSession::PTR& session,
+                                const char* buffer, size_t len) {
         size_t retlen = 0;
 
         if (httpParser->isWebSocket())
-        {
             retlen = HttpService::ProcessWebSocket(buffer, len, httpParser, httpSession);
-        }
         else
-        {
             retlen = HttpService::ProcessHttp(buffer, len, httpParser, httpSession);
-        }
 
         return retlen;
     });
